e4adbfde60ad34dfc704d71e4d173b300d51bb1d
matthilt <32342794+matthilt@users.noreply.github.com>
2018-03-30 15:26:19 -0400

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#Check if the minecraft server is running
+
+RESULT=`ps aux | grep "[m]inecraft_server.jar"`
+if [ "${RESULT:-null}" = null ]; then #if the RESULT from the previous command is null
+echo "Minecraft Server is not running, not backing up."
+exit 1
+else
+
+#Let users know of the backup and turn off world saving temporarily
+/usr/bin/screen -p 0 -S minecraft -X eval 'stuff "say Server Backup Beginning Now..."\\015save-off\\015save-all\\0$
+sleep 3
+
+#change working directory to my hourly backup folder
+cd /home/minecraft
+
+#do level backup
+zip -9 -r backup/Minecraft-Server.zip server
+mv -f backup/Minecraft-Server.zip backup/Minecraft-Server-$(date -d "today" +"%Y%m%d").zip
+
+#Let users know the backup is done and re-enable world saving. Also relay the time, because why not.
+/usr/bin/screen -p 0 -S minecraft -X eval 'stuff save-on\\015"say Backup complete. World now saving to disk."\\015'
+
+/usr/bin/screen -p 0 -S minecraft -X eval 'stuff "say Beginning Cloud upload of backup"\\015'
+/home/matthilt/gdrive upload -p 1YoH2OH8sTkHNIO6s4XjG7x6zp1KxGJbt backup/Minecraft-Server-$(date -d "today" +"%Y%m$
+/usr/bin/screen -p 0 -S minecraft -X eval 'stuff "say Cloud upload of backup complete"\\015'
+
+fi
+exit 0
