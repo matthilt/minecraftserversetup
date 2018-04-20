4c3631aa44215899e78d0f518181e8c04c05b49a
Matt Hilton <matthilt@gmail.com>
2018-04-20 19:47:17 -0400

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -12,16 +12,18 @@ else
 /usr/bin/screen -p 0 -S minecraft -X eval 'stuff "say Server Backup Beginning Now..."\\015save-off\\015save-all\\015'
 sleep 3
 
+#Change Directory into home directory
+cd /home/###USER###
+
 #do level backup
-zip -9 -r /home/###USER###/backup/Minecraft-Server.zip /home/###USER###/server
-mv -f /home/###USER###/backup/Minecraft-Server.zip /home/###USER###/backup/Minecraft-Server-$(date -d "today" +"%Y%m%d").zip
+zip -9 -r backup/Minecraft-Server.zip server
+mv -f backup/Minecraft-Server.zip backup/Minecraft-Server-$(date -d "today" +"%Y%m%d").zip
 
 #Let users know the backup is done and re-enable world saving. Also relay the time, because why not.
 /usr/bin/screen -p 0 -S minecraft -X eval 'stuff save-on\\015"say Backup complete. World now saving to disk."\\015'
 
 /usr/bin/screen -p 0 -S minecraft -X eval 'stuff "say Beginning Cloud upload of backup"\\015'
-azcopy --source backup/Minecraft-Server-$(date -d "today" +"%Y%m%d").zip --destination https://mcserverbackup.blob.core.windows.net/mcserverbackup-container/backups/Minecraft-Server-$(date -d "today" +"%Y%m%d").zip --dest-key lJOLgGK54GBEOPa4apKDtnO7XbJIFvUWESZhMpcilr3jTqC1JNGZSsozfn2mD5AAxGbl5FTgXObcAsHQ04e0cw==
-#/home/###USER###/gdrive upload -p 1YoH2OH8sTkHNIO6s4XjG7x6zp1KxGJbt backup/Minecraft-Server-$(date -d "today" +"%Y%m%d").zip
+azcopy --source backup/Minecraft-Server-$(date -d "today" +"%Y%m%d").zip --destination https://mcserverbackup.blob.core.windows.net/mcserverbackup-container/backups/Minecraft-Server-$(date -d "today" +"%Y%m%d").zip --dest-key lJOLgGK54GBEOPa4apKDtnO7XbJIFvUWESZhMpcilr3jTqC1JNGZSsozfn2mD5AAxGbl5FTgXObcAsHQ04e0cw== --quiet
 /usr/bin/screen -p 0 -S minecraft -X eval 'stuff "say Cloud upload of backup complete"\\015'
 
 fi
